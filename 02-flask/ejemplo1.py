053edce4c7dceb324a66e906e704d320079ce5a7
José <63377749+jrm2087@users.noreply.github.com>
2021-11-27 21:10:44 -0500

    ejercicios flask basico

--- 02-flask/ejemplo1.py
+++ 02-flask/ejemplo1.py
@@ -15,8 +15,24 @@ def salir():
 
 @app.route('/saludar/<nombre>')
 def saludar(nombre):
-    return f'<h2>hola {nombre}, buenos dias</h2>'
+    return f'posicion 5 del nombre es {nombre[5]}'
+    # return f'<h2>hola {nombre}, buenos dias</h2>'
+
+
+@app.route('/longitud/<palabra>')
+def logintud(palabra):
+    return f'La longitud de la palabra "{palabra}" es: {len(palabra)}'
+
+
+@app.route('/edad/<nombre>/<edad>')
+def edad(nombre: str, edad):
+    return f'{nombre.upper()} tiene {edad}'
+
+
+@app.route('/sumar/<num1>/<num2>')
+def sumar(num1, num2):
+    return f'La suma de {num1} y {num2} es {int(num1) + int(num2)}'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
