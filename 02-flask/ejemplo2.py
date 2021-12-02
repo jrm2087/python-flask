6dc0a98b939ded78ac04e801c2274f5c0ac3d9d2
José <63377749+jrm2087@users.noreply.github.com>
2021-12-01 21:45:30 -0500

    templates

---
+++ 02-flask/ejemplo2.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def portada():
+    titulo = 'Inicio'
+    return render_template('portada.html', title=titulo)
+
+
+@app.route("/hola")
+def hola():
+    valores = {'nombre': 'José', 'edad': 34, 'color': 'Negro'}
+    return render_template('hola.html', datos=valores)
+
+
+@app.route('/colores')
+def colores():
+    colores = ['negro', 'gris', 'amarillo', 'azul', 'rojo']
+    return render_template('colores.html', colores=colores)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
