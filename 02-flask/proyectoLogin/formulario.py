e75172ce52d6ecca226baa01eb814cd4a8d84db9
José <63377749+jrm2087@users.noreply.github.com>
2021-12-13 20:18:19 -0500

    clase formulario

---
+++ 02-flask/proyectoLogin/formulario.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from modelos import Usuario
+
+
+class FormularioRegistro(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    nombre = StringField('Nombre', validators=[DataRequired()])
+    password = PasswordField('Password',
+                             validators=[DataRequired(), EqualTo('password_repetir', message='Contraseñas diferentes')])
+    password_repetir = PasswordField('Repetir Password', validators=[DataRequired()])
+    boton = StringField('Registrar')
+
+    def verificar_mail(self, parametro):
+        if Usuario.query.filter_by(email=parametro.data).first():
+            raise ValidationError('Error, este mail ya ha sido utilizado')
+
+    def verificar_nombre(self, parametro):
+        if Usuario.query.filter_by(nombre=parametro.data).first():
+            raise ValidationError('Error, este nombre ya ha sido utilizado')
+
+
+class FormularioLogin(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    boton = StringField('Entrar')
